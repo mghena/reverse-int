8edaef2a3088358e6d9e15f8b02a7c6a9daf4acf
Ghenadie Maleca <ghenadiemaleca@Ghenadies-MacBook-Pro.local>
2023-08-19 23:49:19 +0300

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const reversedNum = n.toString();
+  const stringNum = reversedNum.split('');
+  const reversedArr = stringNum.reverse();
+  const reversedString = reversedArr.join('');
+  const result = parseInt(reversedString);
+  return result;
 }
